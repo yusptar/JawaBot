fe65e1cb193aab2d416bf0f665924b666d65b56b
yusptar <yusupvatar@gmail.com>
2022-11-01 07:08:16 +0700

    update onboarding page

--- lib/models/onboarding.dart
+++ lib/models/onboarding.dart
@@ -10,16 +10,16 @@ List<OnboardingModel> tabs = [
   OnboardingModel(
     'assets/plant.json',
     'Learn JawaBot',
-    'When you order Eat Street , \nwe\'ll hook you up with exclusive \ncoupons.',
+    'Example Text.',
   ),
   OnboardingModel(
     'assets/people-jump.json',
     'Study Together',
-    'We make it simple to find the \nfood you crave. Enter your \naddress and let',
+    'Example Text.',
   ),
   OnboardingModel(
     'assets/chat-bubble.json',
     'Chat with JawaBot',
-    'We make food ordering fast ,\n simple and free - no matter if you \norder',
+    'Example Text.',
   ),
 ];
